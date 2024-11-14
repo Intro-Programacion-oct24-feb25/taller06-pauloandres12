807996daae8405b831f2dd360e4b23fbfcd0299b
Paulo Andres <pbustamante@utpl.edu.ec>
2024-11-14 12:55:03 -0500

    Problema02

---
+++ problema02/problema02/src/problema02/Problema02.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema02;
+import java.util.Scanner;
+/**
+ *
+ * @author utpl
+ */
+public class Problema02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        String nombre;
+        double vehiculo;
+        double valor;
+        double peaje;
+        int tipo;
+        
+        System.out.println("Ingrese su nombre");
+        nombre = entrada.nextLine();
+        
+        
+        
+        
+        
+    }
+    
+}
